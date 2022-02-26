8d576424021ef3dab5d1d9059ea6ea13191b5f08
Gregory Travis <greg.m.travis@gmail.com>
2022-02-26 14:04:36 -0500

    lots

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+// use std::cmp::min;
 use std::convert::TryInto;
 use std::fs::File;
 use std::io::BufWriter;
-// use std::path::Path;
 
-#[derive(Clone, Copy)]
+const BLACK: Pixel = Pixel { r: 0, g: 0, b: 0, a: 255 };
+const BLACKT: Pixel = Pixel { r: 0, g: 0, b: 0, a: 128 };
+const NONE: Pixel = Pixel { r: 0, g: 0, b: 0, a: 0 };
+const REDT: Pixel = Pixel { r: 255, g: 0, b: 0, a: 128 };
+
+pub fn length(a: f32, b: f32) -> f32 {
+  (a*a + b*b).sqrt()
+}
+
+#[derive(Clone, Copy, Debug)]
 pub struct Pixel {
   pub r: u8,
   pub g: u8,
@@ -11,10 +20,40 @@ pub struct Pixel {
   pub a: u8,
 }
 
+// OVER Pixel { r: 0, g: 0, b: 0, a: 255 } Pixel { r: 0, g: 0, b: 0, a: 255 } Pixel { r: 0, g: 0, b: 0, a: 0 }
+// OVER Pixel { r: 0, g: 0, b: 0, a: 255 } Pixel { r: 0, g: 0, b: 0, a: 255 } Pixel { r: 0, g: 0, b: 0, a: 0 }
+// OVER Pixel { r: 0, g: 0, b: 0, a: 255 } Pixel { r: 0, g: 0, b: 0, a: 255 } Pixel { r: 0, g: 0, b: 0, a: 0 }
+// OVER Pixel { r: 255, g: 0, b: 0, a: 128 } Pixel { r: 0, g: 0, b: 0, a: 255 } Pixel { r: 255, g: 0, b: 0, a: 0 }
+// OVER Pixel { r: 255, g: 0, b: 0, a: 255 } Pixel { r: 0, g: 0, b: 0, a: 128 } Pixel { r: 255, g: 0, b: 0, a: 255 }
+// a over b
+pub fn over(a: &Pixel, b: &Pixel) -> Pixel {
+  let ao = (a.a as f32 + (b.a as f32 * (255.0 - a.a as f32))).clamp(0.0, 255.0);
+  let over = Pixel {
+    a: ao as u8,
+    r: (((a.r as f32 * a.a as f32) + (b.r as f32 * b.a as f32 * (255.0 - a.a as f32))) / ao).clamp(0.0, 255.0)  as u8,
+    g: (((a.g as f32 * a.a as f32) + (b.g as f32 * b.a as f32 * (255.0 - a.a as f32))) / ao).clamp(0.0, 255.0)  as u8,
+    b: (((a.b as f32 * a.a as f32) + (b.b as f32 * b.a as f32 * (255.0 - a.a as f32))) / ao).clamp(0.0, 255.0)  as u8,
+  };
+  // println!("OVER {:?} {:?} {:?}", a, b, over);
+  over
+}
+
+#[derive(Clone, Copy)]
+pub struct Pt<T> {
+  pub x: T,
+  pub y: T,
+}
+
+#[derive(Clone, Copy)]
+pub struct Rect<T> {
+  pub ll: Pt<T>,
+  pub ur: Pt<T>,
+}
+
 #[allow(unused_variables, dead_code)]
 pub struct FB {
-  w: usize,
-  h: usize,
+  pub w: usize,
+  pub h: usize,
   pixels: Vec<u8>,
 }
 
@@ -30,10 +69,13 @@ impl FB {
 
   pub fn set(&mut self, x: usize, y: usize, pix: &Pixel) {
     let off = ((y*self.w) + x) * 4;
-    self.pixels[off+0] = pix.r;
-    self.pixels[off+1] = pix.g;
-    self.pixels[off+2] = pix.b;
-    self.pixels[off+3] = pix.a;
+    // TODO unnecessary copy
+    let current = Pixel { r: self.pixels[off+0], b: self.pixels[off+1], g: self.pixels[off+2], a: self.pixels[off+3] };
+    let over = over(&pix, &current);
+    self.pixels[off+0] = over.r;
+    self.pixels[off+1] = over.g;
+    self.pixels[off+2] = over.b;
+    self.pixels[off+3] = over.a;
   }
 
   pub fn write(&self, path: String) {
@@ -75,6 +117,169 @@ impl FB {
   // }
 }
 
+fn render(shape: &impl Shape, colorer: fn(f32) -> Pixel, domain: Rect<f32>, fb: &mut FB) {
+  let ox = domain.ll.x;
+  let oy = domain.ll.y;
+  let dx = (domain.ur.x - domain.ll.x) / (fb.w as f32);
+  let dy = (domain.ur.y - domain.ll.y) / (fb.h as f32);
+  for x in 0..fb.w {
+    for y in 0..fb.h {
+      let fx = ox + ((x as f32) * dx);
+      let fy = oy + ((y as f32) * dy);
+      fb.set(x, y, &colorer(shape.dist(fx, fy)));
+    }
+  }
+}
+
+pub trait Shape {
+  fn dist(&self, x: f32, y: f32) -> f32;
+}
+
+// TODO center, radius?
+#[derive(Clone, Copy, Debug)]
+pub struct Circle {
+}
+
+impl Shape for Circle {
+  fn dist(&self, x: f32, y: f32) -> f32 {
+    let d = length(x, y) - 1.0;
+    // println!("dist {} {} {}|", x, y, d);
+    d
+  }
+}
+
+pub struct Translate {
+  shape: Box<dyn Shape>,
+  tx: f32,
+  ty: f32,
+}
+
+impl Translate {
+  pub fn new(shape: Box<dyn Shape>, tx: f32, ty: f32) -> Translate {
+    Translate { shape: shape, tx: tx, ty: ty }
+  }
+}
+
+impl Shape for Translate {
+  fn dist(&self, x: f32, y: f32) -> f32 {
+    self.shape.dist(x - self.tx, y - self.ty)
+  }
+}
+
+pub struct Union {
+  shape0: Box<dyn Shape>,
+  shape1: Box<dyn Shape>,
+}
+
+impl Union {
+  pub fn new(shape0: Box<dyn Shape>, shape1: Box<dyn Shape>) -> Union {
+    Union { shape0: shape0, shape1: shape1 }
+  }
+}
+
+impl Shape for Union {
+  fn dist(&self, x: f32, y: f32) -> f32 {
+    self.shape0.dist(x, y).min(self.shape1.dist(x, y))
+  }
+}
+
+pub struct Intersection {
+  shape0: Box<dyn Shape>,
+  shape1: Box<dyn Shape>,
+}
+
+impl Intersection {
+  pub fn new(shape0: Box<dyn Shape>, shape1: Box<dyn Shape>) -> Intersection {
+    Intersection { shape0: shape0, shape1: shape1 }
+  }
+}
+
+impl Shape for Intersection {
+  fn dist(&self, x: f32, y: f32) -> f32 {
+    self.shape0.dist(x, y).max(self.shape1.dist(x, y))
+  }
+}
+
+pub struct Difference {
+  shape0: Box<dyn Shape>,
+  shape1: Box<dyn Shape>,
+}
+
+impl Difference {
+  pub fn new(shape0: Box<dyn Shape>, shape1: Box<dyn Shape>) -> Difference {
+    Difference { shape0: shape0, shape1: shape1 }
+  }
+}
+
+impl Shape for Difference {
+  fn dist(&self, x: f32, y: f32) -> f32 {
+    self.shape0.dist(x, y).max(-self.shape1.dist(x, y))
+  }
+}
+
+pub struct SmoothUnion {
+  shape0: Box<dyn Shape>,
+  shape1: Box<dyn Shape>,
+}
+
+impl SmoothUnion {
+  pub fn new(shape0: Box<dyn Shape>, shape1: Box<dyn Shape>) -> SmoothUnion {
+    SmoothUnion { shape0: shape0, shape1: shape1 }
+  }
+}
+
+impl Shape for SmoothUnion {
+  fn dist(&self, x: f32, y: f32) -> f32 {
+    let d0 = self.shape0.dist(x, y);
+    let d1 = self.shape1.dist(x, y);
+    length((d0-0.1).min(0.0), (d1-0.1).min(0.0))
+  }
+}
+
+// float round_merge(float shape1, float shape2, float radius){
+//     float2 intersectionSpace = float2(shape1, shape2);
+//     intersectionSpace = min(intersectionSpace, 0);
+//     return length(intersectionSpace);
+// }
+
+pub struct Hmm {
+  shape0: Box<dyn Shape>,
+  shape1: Box<dyn Shape>,
+}
+
+impl Hmm {
+  pub fn new(shape0: Box<dyn Shape>, shape1: Box<dyn Shape>) -> Hmm {
+    Hmm { shape0: shape0, shape1: shape1 }
+  }
+}
+
+impl Shape for Hmm {
+  fn dist(&self, x: f32, y: f32) -> f32 {
+    self.shape0.dist(x, y) + self.shape1.dist(x, y)
+  }
+}
+
+// Turn a distance into a color
+fn _solid(d: f32) -> Pixel {
+  let inside = d > 0.0;
+  if inside { REDT } else { BLACK }
+}
+
+// Turn a distance into a color
+const BANDWIDTH: f32 = 0.1;
+fn band(d: f32) -> Pixel {
+  let inside = d.abs() < (BANDWIDTH/2.0);
+  if inside { REDT } else { NONE }
+}
+
+// fn compose<A, B, C, G, F>(f: F, g: G) -> impl Fn(A) -> C
+// where
+//     F: Fn(A) -> B,
+//     G: Fn(B) -> C,
+// {
+//     move |x| g(f(x))
+// }
+
 fn main() {
     println!("Hello, world!");
   // let path = Path::new(r"image.png");
@@ -100,15 +305,36 @@ fn main() {
   //             0, 255, 0, 255, 0, 0, 255, 255]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.
   // let data = fb.get_raw::<N>();
 
-  let mut fb: FB = FB::new(2, 2);
-  let red = Pixel { r: 255, g: 0, b: 0, a: 255 };
-  let green = Pixel { r: 0, g: 255, b: 0, a: 255 };
-  let blue = Pixel { r: 0, g: 0, b: 255, a: 255 };
-  let white = Pixel { r: 255, g: 255, b: 255, a: 255 };
-  fb.set(0, 0, &green);
-  fb.set(0, 1, &red);
-  fb.set(1, 0, &blue);
-  fb.set(1, 1, &white);
-  fb.write("image.png".to_string());
+  // let mut fb: FB = FB::new(2, 2);
+  // let red = Pixel { r: 255, g: 0, b: 0, a: 255 };
+  // let green = Pixel { r: 0, g: 255, b: 0, a: 255 };
+  // let blue = Pixel { r: 0, g: 0, b: 255, a: 255 };
+  // let white = Pixel { r: 255, g: 255, b: 255, a: 255 };
+  // fb.set(0, 0, &green);
+  // fb.set(0, 1, &red);
+  // fb.set(1, 0, &blue);
+  // fb.set(1, 1, &white);
+  // // fb.write("image.png".to_string());
 
+  let (w, h) = (800, 800);
+  // let (w, h) = (20, 20);
+  // let (w, h) = (2, 2);
+  let circle = Circle {};
+  let moved = Translate::new(Box::new(circle), 1.0, 0.0);
+  let moved2 = Translate::new(Box::new(circle), 1.0, 0.0);
+  let moved3 = Translate::new(Box::new(circle), 1.0, 0.0);
+  let moved4 = Translate::new(Box::new(circle), 1.0, 0.0);
+  let moved5 = Translate::new(Box::new(circle), 1.0, 0.0);
+  let inter = Intersection::new(Box::new(circle), Box::new(moved));
+  let union = Union::new(Box::new(circle), Box::new(moved2));
+  let diff = Difference::new(Box::new(circle), Box::new(moved4));
+  let hmm = Hmm::new(Box::new(circle), Box::new(moved3));
+  let smooth = SmoothUnion::new(Box::new(circle), Box::new(moved5));
+  let mut cfb = FB::new(w, h);
+  // render(&inter, band, Rect { ll: Pt { x: -2.0, y: -2.0 }, ur: Pt { x: 2.0, y: 2.0 } }, &mut cfb);
+  // render(&union, band, Rect { ll: Pt { x: -2.0, y: -2.0 }, ur: Pt { x: 2.0, y: 2.0 } }, &mut cfb);
+  // render(&hmm, band, Rect { ll: Pt { x: -2.0, y: -2.0 }, ur: Pt { x: 2.0, y: 2.0 } }, &mut cfb);
+  // render(&diff, band, Rect { ll: Pt { x: -2.0, y: -2.0 }, ur: Pt { x: 2.0, y: 2.0 } }, &mut cfb);
+  render(&smooth, band, Rect { ll: Pt { x: -2.0, y: -2.0 }, ur: Pt { x: 2.0, y: 2.0 } }, &mut cfb);
+  cfb.write("image.png".to_string());
 }
